e6c036b67430b0b6dd21322861685ce03b57fed8
Hamza Amar <rhamzax@gmail.com>
2023-03-07 19:26:13 -0700

    Create routes for different pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import Home from "./pages/Home";
+import Edit from "./pages/Edit";
+
 function App() {
-  return <h1>Lotion</h1>;
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to ="/notes" />}>
+        </Route>
+        <Route path="/notes" element={<Home />}></Route>
+        <Route path="/edit" element={<Edit />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
